553844d9a2a7135aca17d1570d189405389475c0
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2021-12-14 17:51:00 +0500

    Section 8 : Restaurant App screeen design setting up search bar

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState("");
   return (
-    <View>
-      <Text>Welcome</Text>
+    <View style={styles.background}>
+      <SearchBar
+        term={term}
+        onTermChange={(value) => {
+          setTerm(value);
+        }}
+        onTermSubmit={() => {
+          console.log("welcome");
+        }}
+      />
     </View>
   );
 };
