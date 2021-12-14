712b308f571548416d64d50c63c8bed8fedcd5e1
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2021-12-14 19:41:12 +0500

    Section 9 : Working with APIs

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,20 +1,40 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [errmess, setErrMess] = useState("");
+
+  const searchApi = async (searchTerm) => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "san jose",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      console.log(err, "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+      setErrMess("something went wrong");
+    }
+  };
+
   return (
     <View style={styles.background}>
       <SearchBar
         term={term}
-        onTermChange={(value) => {
-          setTerm(value);
-        }}
+        onTermChange={setTerm}
         onTermSubmit={() => {
-          console.log("welcome");
+          searchApi(term);
         }}
       />
+      {errmess ? <Text>{errmess}</Text> : null}
+      <Text>Hello {results.length}</Text>
     </View>
   );
 };
